5845232c5f7f1b8ae924952fcd8a24e23af10db7
井上 翔太 <631101@np.group>
2025-09-08 14:32:10 +0900

    認証済みのみ許可

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,19 +3,8 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'amplifyTeamDrive',
   access: (allow) => ({
-    "public/*": [
-      allow.guest.to(["read", "write", "delete"]),
-      allow.authenticated.to(["read", "write", "delete"]),
-    ],
-    "public_readonly/*": [
-      allow.guest.to(["read"]),
-      allow.authenticated.to(["read", "write", "delete"]),
-    ],
     "authenticated/*": [
       allow.authenticated.to(["read"]),
-    ],
-    "private/{entity_id}/*": [
-      allow.entity("identity").to(["read", "write", "delete"]),
-    ],
+    ]
   }),
 });
